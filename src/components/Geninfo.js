6827bd5749d71487d0c3c1ab2bf5ccb7acc34c92
d-emrys <omayr010@gmail.com>
2022-04-18 15:48:54 +0100

    2 files modified

--- src/components/Geninfo.js
+++ src/components/Geninfo.js
@@ -4,6 +4,7 @@ import React from 'react' ;
 function Gen() {
 return(<div className="geni" >
   <big>Qaasim OMar</big>
+  <br/>
 <p> FrontEnd Developer</p>
 <p>Gamer And A Techie </p>
 <p><a href="">Omarqaasim.netlify.com</a></p>
