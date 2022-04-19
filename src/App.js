c75e47b60ee74873cd97738794bc6749dc191e11
d-emrys <omayr010@gmail.com>
2022-04-19 11:25:35 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Education from "./components/Education.js" ;
 import Footer from "./components/Footer.js"; 
 import Summary from "./components/Summary.js" ;
 
-import "./components/Pp.jpg" ;
+import "./Pp.jpg" ;
 
 
 
