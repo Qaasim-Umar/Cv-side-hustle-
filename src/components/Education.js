fee71296790dbbaab9bceada86ac36b1b1acc4f0
d-emrys <omayr010@gmail.com>
2022-04-19 10:36:02 +0100

    1 file modified

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 
 function edu() {
-  return(<div className="contact" >
+  
+  return(<div className="Experience" >
+    <hr/>
+    <h3>EXPERIENCE</h3>
+    <hr/>
 <p>Omayr010@gmail.com</p>
 <p>9121573754 </p>
 <p>Remote</p>
